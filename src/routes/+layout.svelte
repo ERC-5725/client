ff81d9458ae745d922c2f648392ae89ddc595fcb
Elliott Green <elliottgreen@live.com>
2023-11-11 04:59:55 +0000

    [chore] resolve static image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,9 +17,9 @@
 				<strong class="text-xl uppercase">ERC5725</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<!-- <a class="btn btn-sm variant-ghost-surface" href="/" target="_blank" rel="noreferrer">
+				<a class="btn btn-sm variant-ghost-surface" href="/" target="_blank" rel="noreferrer">
 					Home
-				</a> -->
+				</a>
 				<a
 					class="btn btn-sm variant-ghost-surface"
 					href="https://eips.ethereum.org/assets/eip-5725/"
