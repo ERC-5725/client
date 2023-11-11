4e8540275f6804fb84397585cd0dddc423b67846
Elliott Green <elliottgreen@live.com>
2023-11-11 05:02:25 +0000

    [chore] resolve unbased static link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,8 @@
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	import { base } from '$app/paths';
 </script>
 
 <!-- App Shell -->
@@ -17,7 +19,7 @@
 				<strong class="text-xl uppercase">ERC5725</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a class="btn btn-sm variant-ghost-surface" href="/" target="_blank" rel="noreferrer">
+				<a class="btn btn-sm variant-ghost-surface" href="{base}/" target="_blank" rel="noreferrer">
 					Home
 				</a>
 				<a
